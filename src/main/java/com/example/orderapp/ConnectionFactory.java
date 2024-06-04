b047bd83ec2056bd8b68b0797f450f5779e24520
Pasquale Ruotolo <pachiruo@gmail.com>
2024-06-04 15:09:00 +0200

    .

--- src/main/java/com/example/orderapp/ConnectionFactory.java
+++ src/main/java/com/example/orderapp/ConnectionFactory.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    private static final String DB_URL = "jdbc:mysql://database-1.cfcoik2qycvy.eu-north-1.rds.amazonaws.com:3306/restaurant";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/restaurant";
     private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "56265626";
+    private static final String DB_PASSWORD = "562656";
 
     public static Connection createConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
